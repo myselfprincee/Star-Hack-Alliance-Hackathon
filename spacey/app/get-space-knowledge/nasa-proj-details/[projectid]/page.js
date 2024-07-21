a9ccacb6b4f84a556f7d9c3d6b64fffb10a03775
Prince Gupta <prince.kumar01129@gmail.com>
2024-07-22 03:46:54 +0530

    changed proxy

--- spacey/app/get-space-knowledge/nasa-proj-details/[projectid]/page.js
+++ spacey/app/get-space-knowledge/nasa-proj-details/[projectid]/page.js
@@ -15,7 +15,7 @@ const ProjectID = () => {
         const fetchData = async () => {
             setLoading(true);
             try {
-                const response = await fetch(`https://thingproxy.freeboard.io/fetch/https://techport.nasa.gov/api/projects/${projectId}`);
+                const response = await fetch(`https://test.cors.workers.dev/?https://techport.nasa.gov/api/projects/${projectId}`);
 
                 if (!response.ok) {
                     throw new Error(`HTTP error! Status: ${response.status}`);
